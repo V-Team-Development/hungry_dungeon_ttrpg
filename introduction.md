b921e437a7c8b8073d439380728383b46469a53d
pastelstoic <pastelstoic@proton.me>
2024-12-13 11:54:02 -0500

    Added web deployment

--- introduction.md
+++ introduction.md
@@ -2,3 +2,6 @@
 
 Welcome to Hungry Dungeon! This guidebook aims to introduce you to the mechanics
 of the game in a way accessable to newcomers.
+
+To start playing, first go
+[Create a Character](characters/character_creation.md#character-creation).
---
+++ main.ts
@@ -0,0 +1,57 @@
+import { CSS, render } from "@deno/gfm";
+
+async function formatPage(req: Request) {
+  const url = new URL(req.url);
+  let path = url.pathname;
+
+  const sectionJump = path.indexOf("#");
+  if (sectionJump > 1) {
+    path = path.substring(0, sectionJump);
+  }
+
+  path = path.substring(1);
+
+  if (path.length <= 1) {
+    path = "./introduction.md";
+  }
+
+  const markdown = await Deno.readTextFile(path);
+  const body = render(markdown, {
+    baseUrl: url.host,
+  });
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        main {
+          max-width: 800px;
+          margin: 0 auto;
+        }
+        ${CSS}
+      </style>
+    </head>
+    <body>
+      <main data-color-mode="light" data-light-theme="light" data-dark-theme="dark" class="markdown-body">
+        ${body}
+      </main>
+    </body>
+  </html>
+  `;
+}
+
+Deno.serve(async (req) => {
+  if (req.url.endsWith("favicon.ico")) {
+    return new Response(null, {
+      status: 404,
+    });
+  }
+  const body = await formatPage(req);
+  return new Response(body, {
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+    },
+  });
+});
